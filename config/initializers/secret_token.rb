b7135d0626cc63d2c4b5da0828d6b2a6d261fe22
Luke Seelenbinder <luke.seelenbinder@gmail.com>
2012-03-01 16:13:16 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DealerOnTheGo::Application.config.secret_token = '1fe49347ff268c988e446eb76f6ef756075353677c8318f6b019b4d0c2f59a4052d96e1e45109f0d7880768b988a1905567e4a798f82f6c090c8a688d9f51671'
