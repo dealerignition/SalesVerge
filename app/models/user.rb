5fb8501d7390f16e6b3211035e3b5bc1fa38d405
Tony Beninate <tony@dealerignition.com>
2012-03-20 12:35:12 -0400

    [#26733625] Display user's name in navigation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ActiveRecord::Base
   
   belongs_to :dealer
   
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation
 
+  validates_presence_of :first_name
+  validates_presence_of :last_name
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
   validates_presence_of :email
