e3529305a3a524db42a4e3e9f824f60516e1c1e6
Luke Seelenbinder <luke.seelenbinder@gmail.com>
2012-03-29 13:54:49 -0400

    [#26862909] Explicitly blacklist all permissions.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,13 +5,19 @@ class Ability
     if user and user.admin?
       can :manage, :all
     elsif user and user.owner?
+      cannot :manage, :all
+
       can :update, Dealer, :id => user.dealer.id
       can :manage, Customer, :user => { :dealer_id => user.dealer.id }
       can :manage, User, :dealer_id => user.dealer.id
     elsif user and user.salesrep?
+      cannot :manage, :all
+
       can :manage, Customer, :user_id => user.id
       can :manage, User, :id => user.id
     else
+      cannot :manage, :all
+
       can :create, Dealer
       can :create, User
     end
