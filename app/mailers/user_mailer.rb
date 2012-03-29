bcee71e297c78f9a650ce8d4df125102b89d08e8
Tony Beninate <tony@dealerignition.com>
2012-03-29 14:44:45 -0400

    [#25338341] Build daily digest email to go to account owner.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,12 @@ class UserMailer < ActionMailer::Base
       mail(:to => @user.email, :subject => "Welcome to DealerOnTheGo")
   end
   
+  def daily_digest(user)
+    @user      = user
+    @samples   = SampleCheckout.where("created_at > ?", 1.day.ago).all
+    @estimates = Quote.where("created_at > ?", 1.day.ago).all
+    @url       = login_path
+    mail(:to => @user.email, :subject => "Your daily digest")
+  end
+  
 end
