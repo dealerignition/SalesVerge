2fe2b6d524df22cc9d143f7f1b60e3c099752713
Luke Seelenbinder <luke.seelenbinder@gmail.com>
2012-03-29 15:53:09 -0400

    [#26862909] Add authorization to Charges.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,6 +1,7 @@
 class ChargesController < ApplicationController
   before_filter :require_login
   before_filter :confirm_active
+  load_and_authorize_resource
 
   def new
     @quote = Quote.find(params[:quote_id])
@@ -9,17 +10,18 @@ class ChargesController < ApplicationController
   
   def create
     @charge = Charge.new(params[:charge], :date => Time.now)
+
     if @charge.save
       flash[:notice] = "Charge was successfully created."
-      redirect_to :back
     else
       flash[:error] = "Charge was not successfully created. Please try again."
-      redirect_to :back
     end
+
+    redirect_to :back
   end
 
   def edit
-    @charge = Charge.find_by_id(params[:id])
+    @charge = Charge.find(params[:id])
   end
   
   def destroy
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    @models = [Customer, Quote, Sample, SampleCheckout, Appointment]
     if user and user.admin?
       can :manage, :all
     elsif user and user.owner?
@@ -9,14 +10,16 @@ class Ability
 
       can :update, Dealer, :id => user.dealer.id
       can :manage, User, :dealer_id => user.dealer.id
-      can :manage, [Customer, Quote, Sample, SampleCheckout, Appointment], :user => { :dealer_id => user.dealer.id }
-      can :create, [User, Quote, Customer, Sample, SampleCheckout, Appointment]
+      can :manage, Charge, :quote => { :user => { :dealer_id => user.dealer.id } }
+      can :manage, @models, :user => { :dealer_id => user.dealer.id }
+      can :create, [User] + @models
     elsif user and user.salesrep?
       cannot :manage, :all
 
       can :manage, User, :id => user.id
-      can :manage, [Customer, Quote, Sample, SampleCheckout, Appointment], :user_id => user.id
-      can :create, [Quote, Customer, Sample, SampleCheckout, Appointment]
+      can :manage, Charge, :quote => { :user_id => user.id }
+      can :manage, @models, :user_id => user.id
+      can :create, @models
     else
       cannot :manage, :all
 
