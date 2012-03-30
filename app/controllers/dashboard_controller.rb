0ccefeae27ac55b863ad0c43f2b2e32859478c5d
Justin Wilson <justin@dealerignition.com>
2012-03-30 16:14:32 -0400

    [#27311961] Uses CanCan to show only things that are accessible_by a specific user on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,9 +5,10 @@ class DashboardController < ApplicationController
   skip_authorization_check
   
   def index
-    @customers = Customer.order("created_at ASC").last(5)
-    @late_appointments = Appointment.where("date < ? AND status NOT LIKE ?", Date.today, "Completed").all
-    @checked_out_samples = SampleCheckout.find_all_by_checkin_time(nil)
+    @customers = Customer.accessible_by(current_ability).order("created_at ASC").last(5)
+    @late_appointments = Appointment.accessible_by(current_ability).where("date < ? AND status NOT LIKE ?", Date.today, "Completed").all
+    @checked_out_samples = SampleCheckout.accessible_by(current_ability).find_all_by_checkin_time(nil)
     @quotes = Quote.order("created_at DESC").last(5)
   end
+  
 end
