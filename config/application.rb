6c72c261c22c1a729633fe1700a56ad52d6fb7eb
Justin Wilson <justin@dealerignition.com>
2012-04-04 12:59:30 -0400

    [#27522045] adds simple gem called "rodie" that automatically puts css inline

--- config/application.rb
+++ config/application.rb
@@ -57,5 +57,8 @@ module DealerOnTheGo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.action_mailer.raise_delivery_errors = true
+    
   end
 end
