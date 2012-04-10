b9a749176af0b7032eb614cca647bc4f11f11e27
Luke Seelenbinder <luke.seelenbinder@gmail.com>
2012-04-10 14:38:59 -0400

    [#27795003] Don't show flash notice on inital attempt to log in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   check_authorization
   protect_from_forgery
   layout :setup_layout
-  
+
   def confirm_active
     @user = current_user if current_user
     if !@user.active?
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
-  
+
   def require_owner
     @user = current_user if current_user
     if @user.role != "owner"
@@ -19,25 +19,25 @@ class ApplicationController < ActionController::Base
       redirect_to account_settings_account_path
     end
   end
-  
+
   private
-  
+
   def not_authenticated
     redirect_to login_url
-    flash[:error] = "You must login to access this page."
+    flash[:error] = "You must login to access this page." unless request.path.eql? "/"
   end
 
   rescue_from CanCan::AccessDenied do |exception|
     request.env["HTTP_REFERER"] ||= dashboard_path
     begin
-      msg = exception.subject.class.model_name.downcase! 
+      msg = exception.subject.class.model_name.downcase!
     rescue
       msg = "page"
     end
 
     redirect_to :back, :flash => { :error => "You are not authorized to access this #{msg}." }
   end
-  
+
   def setup_layout
     return "main" unless current_user
     current_user.salesrep? ? "sales_rep" : "main"
