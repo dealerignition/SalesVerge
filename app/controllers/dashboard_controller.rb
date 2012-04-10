0d4e903901777fab4fd9384bc8fbd71346ec5df5
Tony Beninate <tony@dealerignition.com>
2012-04-10 12:48:20 -0400

    [#27655707] Setup salesrep dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,6 +9,21 @@ class DashboardController < ApplicationController
     @late_appointments = Appointment.accessible_by(current_ability).where("date < ? AND status NOT LIKE ?", Date.today, "Completed").all
     @checked_out_samples = SampleCheckout.accessible_by(current_ability).find_all_by_checkin_time(nil)
     @estimates = Estimate.accessible_by(current_ability).order("created_at DESC").last(5)
+
+    # For SalesRep view
+    if current_user.salesrep?
+      @new_customers = Customer.where("created_at > ?", Date.today.beginning_of_day).accessible_by(current_ability).count
+      @new_checked_out_samples = SampleCheckout.where("created_at > ?", Date.today.beginning_of_day).accessible_by(current_ability).count
+      @new_estimates = Estimate.where("created_at > ?", Date.today.beginning_of_day).accessible_by(current_ability).count
+    end
+    
+    @timeline_stream = []
+    @customers.each do |customer|
+      @timeline_stream += customer.estimates + customer.sample_checkouts
+      @timeline_stream.sort! do |a,b|
+        -(a.updated_at <=> b.updated_at)
+      end
+    end
   end
   
 end
