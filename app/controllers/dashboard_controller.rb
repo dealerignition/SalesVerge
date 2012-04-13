9eaeeaa703f87f16f1624956103cbfd0e6c49d0e
Tony Beninate <tony@dealerignition.com>
2012-04-13 09:09:48 -0400

    [#27983931] Fix a small bug where dashboard could not be viewed as an admin.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -17,23 +17,19 @@ class DashboardController < ApplicationController
     @date_range = params[:date_range].to_s.to_sym
     date_query = "updated_at #{dates[@date_range] || dates[:all_time]}"
 
-    @customers = Customer.accessible_by(current_ability)
-                        .order("created_at ASC")
-                        .where(date_query)
-    @late_appointments = Appointment.accessible_by(current_ability)
-                        .where("date < ? AND status NOT LIKE ?", Date.today, "Completed")
-                        .all
-    @checked_out_samples = SampleCheckout.accessible_by(current_ability)
-                        .find_all_by_checkin_time(nil)
-    @estimates = Estimate.accessible_by(current_ability)
-                        .order("created_at DESC")
-                        .last(5)
+    if current_user.salesrep?
+      @customers = Customer.accessible_by(current_ability).order("created_at ASC").where(date_query)
+    else
+      @customers = Customer.accessible_by(current_ability).order("created_at ASC")
+    end
+      @late_appointments = Appointment.accessible_by(current_ability).where("date < ? AND status NOT LIKE ?", Date.today, "Completed").all
+      @checked_out_samples = SampleCheckout.accessible_by(current_ability).find_all_by_checkin_time(nil)
+      @estimates = Estimate.accessible_by(current_ability).order("created_at DESC").last(5)
 
     @timeline_stream = @customers
     Customer.accessible_by(current_ability).each do |customer|
       [:estimates, :sample_checkouts, :notes].each do |field|
-        @timeline_stream += customer.send(field)
-                              .where(date_query)
+        @timeline_stream += customer.send(field).where(date_query)
       end
     end
 
