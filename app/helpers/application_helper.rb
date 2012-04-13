2324ce5ca6077a4cd75fa405042b1160947faf92
Justin Wilson <justin@dealerignition.com>
2012-04-13 14:24:30 -0400

    [#27837569] stylizes the checked-in/checked-out statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,9 +8,9 @@ module ApplicationHelper
       "#{ you_or_they(model.user) } gave #{model.customer.first_name} an <span class='estimate'>estimate</span> for #{ number_to_currency(model.total) }.".html_safe
     elsif model.instance_of? SampleCheckout
       if model.checkin_time?
-        "#{model.customer.first_name} <span class='sample'>checked-in</span> #{ model.sample.name }.".html_safe
+        "#{model.customer.first_name} <span class='sample'>checked-<span class='sample_in'>in</span></span> #{ model.sample.name }.".html_safe
       else
-        "#{model.customer.first_name} <span class='sample'>checked-out</span> #{ model.sample.name }.".html_safe
+        "#{model.customer.first_name} <span class='sample'>checked-<span class='sample_out'>out</span></span> #{ model.sample.name }.".html_safe
       end
     elsif model.instance_of? Note
       "#{ you_or_they(model.user) } created a <span class='note'>note</span> about #{ model.customer.first_name }.".html_safe
