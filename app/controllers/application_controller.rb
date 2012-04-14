d837e66007560dc652a61e3849b5029a09c95469
Luke Seelenbinder <luke.seelenbinder@gmail.com>
2012-04-13 22:38:39 -0400

    Minor refactoring.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
   layout :setup_layout
 
   def confirm_active
-    @user = current_user if current_user
-    if !@user.active?
+    if !current_user.active?
       logout
       flash[:alert] = "Your account has been deactivated. See your account administrator for details."
       redirect_to login_url
@@ -13,33 +12,45 @@ class ApplicationController < ActionController::Base
   end
 
   def require_owner
-    @user = current_user if current_user
-    if @user.role != "owner"
-      flash[:error] = "Only account owners can access that."
+    if current_user.role != "owner"
+      flash[:error] = "Only account owners can see that."
       redirect_to account_settings_account_path
     end
   end
 
+  DATE_RANGES = {
+    :today => "updated_at > TIMESTAMP 'today'",
+    :yesterday => "TIMESTAMP 'today' > updated_at AND updated_at > TIMESTAMP 'yesterday'",
+    :this_month => "updated_at > CURRENT_DATE - INTERVAL '#{Date.today.day} days'",
+    :last_30_days =>  "updated_at > CURRENT_DATE - INTERVAL '30 days'",
+    :all_time => "updated_at > TIMESTAMP '-infinity'"
+  }
+
   private
 
   def not_authenticated
+    flash[:error] = "You must login to see that page." unless request.path == "/"
     redirect_to login_url
-    flash[:error] = "You must login to access this page." unless request.path.eql? "/"
   end
 
   rescue_from CanCan::AccessDenied do |exception|
     request.env["HTTP_REFERER"] ||= dashboard_path
+
     begin
       msg = exception.subject.class.model_name.downcase!
     rescue
       msg = "page"
     end
 
-    redirect_to :back, :flash => { :error => "You are not authorized to access this #{msg}." }
+    redirect_to :back, :flash => { :error => "You do not have permission to see this #{msg}." }
   end
 
   def setup_layout
-    return "main" unless current_user
-    current_user.salesrep? ? "sales_rep" : "main"
+    if current_user && current_user.salesrep?
+      "sales_rep"
+    else
+      "main"
+    end
   end
+
 end
