29cc319db1830ca6cfb258f57d91902bf6867412
Luke Seelenbinder <luke.seelenbinder@gmail.com>
2012-04-13 22:40:28 -0400

    [#28017491] Improve date range handling.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,36 +6,28 @@ class DashboardController < ApplicationController
 
   def index
 
-    dates = {
-      :today => "> TIMESTAMP 'today'",
-      :yesterday => "> TIMESTAMP 'yesterday' AND updated_at < TIMESTAMP 'today'",
-      :this_month => " > CURRENT_DATE - INTERVAL '1 month'",
-      :all_time => "> TIMESTAMP '-infinity'"
-    }
-
-    @date_options = dates.keys
+    @date_options = DATE_RANGES.keys
     @date_range = params[:date_range].to_s.to_sym
-    date_query = "updated_at #{dates[@date_range] || dates[:all_time]}"
+    @date_range = :all_time unless @date_range.in? @date_options
+    @date_range_string = DATE_RANGES[@date_range]
 
-    if current_user.salesrep?
-      @customers = Customer.accessible_by(current_ability).order("created_at ASC").where(date_query)
-    else
-      @customers = Customer.accessible_by(current_ability).order("created_at ASC")
-    end
-      @late_appointments = Appointment.accessible_by(current_ability).where("date < ? AND status NOT LIKE ?", Date.today, "Completed").all
-      @checked_out_samples = SampleCheckout.accessible_by(current_ability).find_all_by_checkin_time(nil)
-      @estimates = Estimate.accessible_by(current_ability).order("created_at DESC").last(5)
+    @customers = Customer.accessible_by(current_ability).order("created_at ASC")
+
+    @customers = @customers.where(@date_range_string) if current_user.salesrep?
+
+    @late_appointments = Appointment.accessible_by(current_ability)
+          .where("date < TIMESTAMP 'today' AND status NOT LIKE 'Completed'").all
+    @checked_out_samples = SampleCheckout.accessible_by(current_ability).find_all_by_checkin_time(nil)
+    @estimates = Estimate.accessible_by(current_ability).order("created_at DESC").last(5)
 
     @timeline_stream = @customers
     Customer.accessible_by(current_ability).each do |customer|
       [:estimates, :sample_checkouts, :notes].each do |field|
-        @timeline_stream += customer.send(field).where(date_query)
+        @timeline_stream += customer.send(field).where(@date_range_string)
       end
     end
 
-    @timeline_stream.sort! do |a,b|
-      -(a.updated_at <=> b.updated_at)
-    end
-  end
+    @timeline_stream.sort! { |a,b| -(a.updated_at <=> b.updated_at) }
+ end
 
 end
