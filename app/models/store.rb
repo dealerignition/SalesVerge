fb008ac2e6b70f6bd205ac46fc223f848c5dad1e
Tony Beninate <tony@dealerignition.com>
2012-04-20 16:39:36 -0400

    [#28355277] Show website in mailers only if the dealer has specified one. Smart add "http" if the user didn't specify.

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,16 @@
 class Store < ActiveRecord::Base
+  before_validation :smart_add_url_protocol
   validates_presence_of :dealer_id
+  
   belongs_to :dealer
   has_many :samples, :dependent => :destroy
+  
+  protected
+
+  def smart_add_url_protocol
+    unless self.website[/^http:\/\//] || self.website[/^https:\/\//]
+      self.website = 'http://' + self.website
+    end
+  end
+  
 end
