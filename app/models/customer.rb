69f42967c66549c6963907a86c24b9e6240c8bf1
Justin Wilson <justin@dealerignition.com>
2012-04-26 17:22:48 -0400

    [#28630313] wire-up display of RapLeaf data in customer show page

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,7 +11,7 @@ class Customer < ActiveRecord::Base
   has_many :quotes, :dependent => :destroy
   has_many :notes, :dependent => :destroy
   
-  has_many :customer_extensions, :dependent => :destroy
+  has_one :customer_extension, :dependent => :destroy
 
   def full_name
     "#{ first_name } #{ last_name }"
