bed5d5d92f3d6a8f56f0269b9ea47eaa2978a64b
Tony Beninate <tony@dealerignition.com>
2012-04-27 15:18:41 -0400

    [#28759151] Switch from GMail to SendGrid.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,11 +3,11 @@ DealerOnTheGo::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
+    :address              => "smtp.sendgrid.net",
     :port                 => 587,
-    :domain               => 'floorstoreonthego.com',
-    :user_name            => ENV['EMAIL_USERNAME'],
-    :password             => ENV['EMAIL_PASSWORD'],
+    :domain               => 'heroku.com',
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
 
