b70995652bcd6ac329380517ce39e78eabc10f6e
Tony Beninate <tony@dealerignition.com>
2012-05-14 10:32:45 -0400

    [#28864469] Drop the use of the "From" header.

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -1,8 +1,7 @@
 class CustomerMailer < ActionMailer::Base
-  default from: "notifications@dealerbookapp.com"
-  
   # NOTE: Every mailer must have a @dealer variable defined as the customer_mailer layout is expecting one
   # NOTE: Every mailer must have a @user variable defined as the signature partial is expecting one
+  
   def thank_you_email(customer)
     @customer = customer
     @user = @customer.user
