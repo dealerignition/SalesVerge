3b9ce74a35b02d0ad017f221333108994cccf764
Tony Beninate <tony@dealerignition.com>
2012-05-14 10:21:26 -0400

    [#28864469] Customer mailers user the user's email as the sender address.

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -3,11 +3,12 @@ class CustomerMailer < ActionMailer::Base
   
   # NOTE: Every mailer must have a @dealer variable defined as the customer_mailer layout is expecting one
   # NOTE: Every mailer must have a @user variable defined as the signature partial is expecting one
-  def thank_you_email(customer, dealer)
+  def thank_you_email(customer)
     @customer = customer
-    @dealer = dealer
+    @user = @customer.user
+    @dealer = @user.dealer
     @url  = "http://floorstoreonthego.com"
-    mail(:to => @customer.email, :subject => "Thank-you for your interest in our store", :reply_to => "#{@customer.user.email}")
+    mail(:sender => @user.email, :to => @customer.email, :subject => "Thank-you for your interest in our store", :reply_to => @customer.user.email)
     
     @sent_email = SentEmail.new(:customer_id => @customer.id, :type => "thank_you")
     @sent_email.save
@@ -18,7 +19,7 @@ class CustomerMailer < ActionMailer::Base
     @user = quote.user
     @customer = quote.customer
     @dealer = quote.user.dealer
-    mail(:to => @customer.email, :subject => "Here is your quote", :reply_to => "#{@customer.user.email}")
+    mail(:sender => @user.email, :to => @customer.email, :subject => "Here is your quote", :reply_to => @user.email)
     
     @sent_email = SentEmail.new(:customer_id => @customer.id, :type => "quote")
     @sent_email.save
@@ -29,7 +30,7 @@ class CustomerMailer < ActionMailer::Base
     @user = quote.user
     @customer = quote.customer
     @dealer = quote.user.dealer
-    mail(:to => @customer.email, :subject => "Thank you for your purchase", :reply_to => "#{@customer.user.email}")
+    mail(:sender => @user.email, :to => @customer.email, :subject => "Thank you for your purchase", :reply_to => @user.email)
     
     @sent_email = SentEmail.new(:customer_id => @customer.id, :type => "quote_won")
     @sent_email.save
@@ -42,7 +43,7 @@ class CustomerMailer < ActionMailer::Base
     @user = sample_checkouts.first.user
     @dealer = @user.dealer
     title = @sample_checkouts.count == 1 ? @sample_checkouts.first.sample.name : "some samples"
-    mail(:to => @customer.email, :subject => "Thank you for checking out #{title}!", :reply_to => "#{@customer.user.email}")
+    mail(:sender => @user.email, :to => @customer.email, :subject => "Thank you for checking out #{title}!", :reply_to => @user.email)
     
     @sent_email = SentEmail.new(:customer_id => @customer.id, :type => "sample_checkout")
     @sent_email.save
@@ -55,7 +56,7 @@ class CustomerMailer < ActionMailer::Base
     @user = sample_checkouts.first.user
     @dealer = @user.dealer
     title = @sample_checkouts.count == 1 ? @sample_checkouts.first.sample.name : "some samples"
-    mail(:to => @customer.email, :subject => "You still have #{title}. Can we get our stuff back?", :reply_to => "#{@customer.user.email}")
+    mail(:sender => @user.email, :to => @customer.email, :subject => "You still have #{title}. Can we get our stuff back?", :reply_to => @user.email)
     sample_checkouts.each do |s|
       s.notifications_received = s.notifications_received + 1
       s.save
