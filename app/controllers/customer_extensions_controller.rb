768244ae96550810eac8dd2853af2868f7956e99
Justin Wilson <justin@dealerignition.com>
2012-05-14 11:35:06 -0400

    [#28959565] adds a button to look-up a customer with RapLeaf if they do not already have a customer_extension

--- app/controllers/customer_extensions_controller.rb
+++ app/controllers/customer_extensions_controller.rb
@@ -1,10 +1,19 @@
 class CustomerExtensionsController < ApplicationController
   
+  skip_authorization_check
+  
   def new
-    @extension = CustomerExtension.new
   end
   
   def create
+    @extension = CustomerExtension.new
+    @extension.customer_id = params[:customer_id]
+    if @extension.save
+      redirect_to :back
+    else
+      flash[:error] = "There was an error."
+      redirect_to :back
+    end
   end
-  
+    
 end
