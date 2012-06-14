890c0207cbb0236056fb3e50a60ed74e5133180e
Tony Beninate <tony@dealerignition.com>
2012-06-14 11:05:19 -0400

    [#31218699] Create forms and add into AppRequest mailer.

--- app/mailers/anonymous_mailer.rb
+++ app/mailers/anonymous_mailer.rb
@@ -3,6 +3,13 @@ class AnonymousMailer < ActionMailer::Base
   
   def app_request(app_request)
     @app_request = app_request
+    if @app_request.app_name == 'DealerOnTheGo'
+      @survey_link = 'https://dealerignition.wufoo.com/forms/dealeronthego-request/'
+    elsif @app_request.app_name == 'FloorStoreOnTheGo'
+      @survey_link = 'https://dealerignition.wufoo.com/forms/floorstoreonthego-request/'
+    elsif @app_request.app_name == 'SalesUps'
+      @survey_link = 'https://dealerignition.wufoo.com/forms/salesups-request/'
+    end
     mail(:to => @app_request.email, :subject => "Thank you for your interest in #{@app_request.app_name}")
   end
 
