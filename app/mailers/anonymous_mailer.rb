77524cb9d47b46af3dbcab5a90cd72f97f0b8ea5
Tony Beninate <tony@dealerignition.com>
2012-06-26 16:33:06 -0400

    [#31842533] Build app invite mailer.

--- app/mailers/anonymous_mailer.rb
+++ app/mailers/anonymous_mailer.rb
@@ -12,5 +12,17 @@ class AnonymousMailer < ActionMailer::Base
     end
     mail(:to => @app_request.email, :subject => "Thank you for your interest in #{@app_request.app_name}")
   end
+  
+  def app_invite(app_request)
+    @app_request = app_request
+    if @app_request.app_name == 'DealerOnTheGo'
+      @survey_link = 'https://dealerignition.wufoo.com/forms/dealeronthego-request/'
+    elsif @app_request.app_name == 'FloorStoreOnTheGo'
+      @survey_link = 'https://dealerignition.wufoo.com/forms/floorstoreonthego-request/'
+    elsif @app_request.app_name == 'SalesUps'
+      @survey_link = 'https://dealerignition.wufoo.com/forms/salesups-request/'
+    end
+    mail(:to => @app_request.email, :subject => "Here is your invitiation to join #{@app_request.app_name}")
+  end
 
 end
