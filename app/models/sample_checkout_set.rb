0357e20548f3467e5065a96acfaa93144b1829c3
Justin Wilson <justin@dealerignition.com>
2012-07-09 14:56:36 -0400

    [#32474969] fix for sql query for send_long_checkout_set_notification method

--- app/models/sample_checkout_set.rb
+++ app/models/sample_checkout_set.rb
@@ -14,9 +14,9 @@ class SampleCheckoutSet < ActiveRecord::Base
                           on user_id = users.id
                         inner join sample_checkouts
                           on sample_checkout_sets.id = sample_checkouts.sample_checkout_set_id
-                        where sample_checkouts.checkin_time is null 
+                        where sample_checkout_sets.checkin_time is null 
                           and (sample_checkout_sets.notifications_received is null or sample_checkout_sets.notifications_received < 1) 
-                          and (current_date - sample_checkouts.checkout_time::date) > users.reminder_interval_days
+                          and (current_date - sample_checkout_sets.checkout_time::date) > users.reminder_interval_days
                         group by sample_checkout_sets.id, sample_checkout_sets.notifications_received, sample_checkout_sets.created_at, 
                           sample_checkout_sets.updated_at, sample_checkout_sets.customer_id, sample_checkout_sets.user_id')
     sample_sets_out.each do |s|
